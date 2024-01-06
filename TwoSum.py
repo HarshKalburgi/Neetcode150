4a68721cbc9384bed8f3b6e71b6bdc6cfb006008
Harsh kalburgi <116815558+HarshKalburgi@users.noreply.github.com>
2024-01-06 23:26:55 +0530

    Add files via upload

---
+++ TwoSum.py
@@ -0,0 +1,14 @@
+#Two Sum
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+class Solution(object):
+    def twoSum(self, nums, target):
+        prevMap = {}  # val: index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return None
