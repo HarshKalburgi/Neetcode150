bd02f56108ed5a4c332730945ceddc1b0bb92a98
Harsh kalburgi <116815558+HarshKalburgi@users.noreply.github.com>
2024-02-13 22:57:06 +0530

    Add files via upload

---
+++ Word Break.py
@@ -0,0 +1,27 @@
+# Word Break
+# Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+# Note that the same word in the dictionary may be reused multiple times in the segmentation.
+# code
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        def construct(current,wordDict, memo={}):
+            if current in memo:
+                return memo[current]
+
+            if not current:
+                return True
+
+            for word in wordDict:
+                if current.startswith(word):
+                    new_current = current[len(word):]
+                    if construct(new_current,wordDict,memo):
+                        memo[current] = True
+                        return True
+
+            memo[current] = False
+            return False
+
+        return construct(s,wordDict)
